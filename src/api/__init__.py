533e832d1ab6eb2df0de1a544eaedf7729b3a468
ruritoBlogger <rurito.blog@gmail.com>
2022-02-10 08:06:29 +0900

    feat: yfinanceを用いたバランスシートの情報を取得する機能を実装

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,5 +1,6 @@
 from .industry import *
 from .company import *
 from .finantialStatements import *
+from .balanceSheet import *
 from .type import *
 from .yfinance import *
--- src/function/generateFinantialStatementsData.py
+++ src/function/generateFinantialStatementsData.py
@@ -1,12 +1,45 @@
-from typing import List
+from typing import Any, List, Tuple
 import sys
 import os
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from api import getCompanyList, addFinantialStatements
-from api.type import AddFinantialStatementsRequstType
-from api.yfinance import getCompanyFSWithYahooAPI
-from domain import FinantialStatements, Company
+from api import yfinance, getCompanyList, addFinantialStatements, addBalanceSheet
+from api.type import AddBalanceSheetRequestType, AddFinantialStatementsRequstType
+from domain import FinantialStatements, Company, BalanceSheet
+
+def generateDataWithYahooAPI() -> List[Tuple[FinantialStatements, BalanceSheet]]:
+    companyList: List[Company] = getCompanyList()
+
+    if type(companyList) is not list:
+        raise ValueError('企業情報の取得に失敗しました')
+
+    elif len(companyList) == 0:
+        raise ValueError('企業情報が登録されていません')
+
+    for company in companyList:
+        ticker: Any = yfinance.getTickerWithYahooAPI(company)
+        dateList: List[str] = yfinance.getCompanyDateWithTicker(ticker)
+
+
+        # 各表のベースとなる財務諸表データを生成
+        fsList: List[FinantialStatements] = []
+        for date in dateList:
+            fs = addFinantialStatements({"companyID": company['id'], "announcementDate": date, "isFiscal": True})
+            fsList.append(fs)
+        
+        # 財務諸表データをベースにしたバランスシートデータを生成
+        bsRequestList: List[AddBalanceSheetRequestType] = yfinance.getCompanyBSWithTicker(ticker, fsList)
+        bsList: List[BalanceSheet] = []
+        for bsRequest in bsRequestList:
+            bs = addBalanceSheet(company['id'], bsRequest)
+            bsList.append(bs)
+        
+        # 財務諸表データをベースにしたキャッシュ・フローデータを生成
+        
+        break
+
+
 
 def generateFinantialStatementsData() -> List[FinantialStatements]:
     companyList: List[Company] = getCompanyList()
@@ -20,7 +53,7 @@ def generateFinantialStatementsData() -> List[FinantialStatements]:
 
     companyListWithFSRequest: List[List[AddFinantialStatementsRequstType]] = []
     for company in companyList:
-        companyListWithFSRequest.append(getCompanyFSWithYahooAPI(company))
+        companyListWithFSRequest.append(yfinance.getCompanyFSWithYahooAPI(company))
 
     result: List[FinantialStatements] = []
     for FSRequstList in companyListWithFSRequest:
@@ -31,4 +64,5 @@ def generateFinantialStatementsData() -> List[FinantialStatements]:
 
 
 if __name__ == "__main__":
-    generateFinantialStatementsData()
+    # generateFinantialStatementsData()
+    generateDataWithYahooAPI()
