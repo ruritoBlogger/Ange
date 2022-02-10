f5285f538d314fc9c34d16fdd25bd36e30421085
ruritoBlogger <ruriro0125@gmail.com>
2022-02-10 10:21:10 +0900

    feat: 現在実装出来る範囲内でIndexを取得する機能を実装した

--- src/function/generateFinantialStatementsData.py
+++ src/function/generateFinantialStatementsData.py
@@ -4,9 +4,24 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from api import yfinance, getCompanyList, addFinantialStatements, addBalanceSheet, addCashFlow, addIncomeStatement
-from api.type import AddBalanceSheetRequestType, AddFinantialStatementsRequstType, AddCashFlowRequestType, AddIncomeStatementRequestType
-from domain import FinantialStatements, Company, BalanceSheet, Cashflow, IncomeStatement
+from api import yfinance, getCompanyList, addFinantialStatements, addBalanceSheet, addCashFlow, addIncomeStatement, addIndex
+from api.type import AddBalanceSheetRequestType, AddFinantialStatementsRequstType, AddCashFlowRequestType, AddIncomeStatementRequestType, AddIndexRequestType
+from domain import FinantialStatements, Company, BalanceSheet, Cashflow, IncomeStatement, Index
+
+
+# TODO: 株価データを扱えるようになったら実装する
+"""
+def calcIndex(ticker, Any, fsList: List[FinantialStatements], bsList: List[BalanceSheet], cfList: List[Cashflow], isList: List[IncomeStatement]) -> List[AddIndexRequestType]:
+    listLen = len(fsList)
+
+    indexRequestList: List[AddIndexRequestType] = []
+    for i in range(listLen):
+        indexRequest: AddIndexRequestType = { "finantialID": fsList[i]['id'] }
+        indexRequest['capitalAdequacyRatio'] = bsList[i]['netAssets'] / bsList[i]['totalAssets']
+        indexRequest['roe'] = isList[i]['netIncome'] / bsList[i]['netAssets'] * 100
+        indexRequest['roa'] = isList[i]['netIncome'] / bsList[i]['totalAssets'] * 100
+"""
+
 
 def generateDataWithYahooAPI() -> List[Tuple[FinantialStatements, BalanceSheet]]:
     companyList: List[Company] = getCompanyList()
@@ -21,14 +36,12 @@ def generateDataWithYahooAPI() -> List[Tuple[FinantialStatements, BalanceSheet]]
         ticker: Any = yfinance.getTickerWithYahooAPI(company)
         dateList: List[str] = yfinance.getCompanyDateWithTicker(ticker)
 
-
         # 各表のベースとなる財務諸表データを生成
         fsList: List[FinantialStatements] = []
         for date in dateList:
             fs = addFinantialStatements({"companyID": company['id'], "announcementDate": date, "isFiscal": True})
             fsList.append(fs)
         
-        """
         # 財務諸表データをベースにしたバランスシートデータを生成
         bsRequestList: List[AddBalanceSheetRequestType] = yfinance.getCompanyBSWithTicker(ticker, fsList)
         bsList: List[BalanceSheet] = []
@@ -42,7 +55,6 @@ def generateDataWithYahooAPI() -> List[Tuple[FinantialStatements, BalanceSheet]]
         for cfRequest in cfRequestList:
             cf = addCashFlow(company['id'], cfRequest)
             cfList.append(cf)
-        """
 
         # 財務諸表データをベースにした損益計算書データを生成
         isRequestList: List[AddIncomeStatementRequestType] = yfinance.getCompanyISWithTicker(ticker, fsList)
@@ -50,6 +62,16 @@ def generateDataWithYahooAPI() -> List[Tuple[FinantialStatements, BalanceSheet]]
         for isRequest in isRequestList:
             istatement = addIncomeStatement(company['id'], isRequest)
             isList.append(istatement)
+
+        # TODO: 株価データを取り扱えるようになったら実装する
+        """
+        # 財務諸表データをベースにした指標データを生成
+        indexRequestList: List[AddIndexRequestType] = calcIndex(fsList, bsList, cfList, isList)
+        indexList: List[Index] = []
+        for indexRequest in indexRequestList:
+            index = addIndex(company['id'], indexRequest)
+            indexList.append(index)
+        """
         
         break
 
