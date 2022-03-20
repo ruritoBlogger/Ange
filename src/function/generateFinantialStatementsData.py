f4287d2307483eacd57adb9b3b058f670756371b
ruritoBlogger <rurito.blog@gmail.com>
2022-03-20 11:33:40 +0900

    feat: 既に登録済の情報を再度登録しないようにした

--- src/function/generateFinantialStatementsData.py
+++ src/function/generateFinantialStatementsData.py
@@ -2,13 +2,15 @@ from typing import Any, List, Tuple, Dict, Optional
 import sys
 import os
 from tqdm import tqdm
+import itertools
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from api import yfinance, getCompanyList, addFinantialStatements, addBalanceSheet, addCashFlow, addIncomeStatement, addIndex, addPrice
+from api import yfinance, getCompanyList, addFinantialStatements, addBalanceSheet, addCashFlow, addIncomeStatement, addIndex, addPrice, getFinantialStatementsList, getBalanceSheetList, getCashFlowList, getIncomeStatementList, getPriceList, getIndexList
 from api.type import AddBalanceSheetRequestType, AddFinantialStatementsRequstType, AddCashFlowRequestType, AddIncomeStatementRequestType, AddIndexRequestType, AddStockPriceRequestType
 from domain import FinantialStatements, Company, BalanceSheet, Cashflow, IncomeStatement, Index, StockPrice
 from util import convertDateFromJS
+from validator import validateFS, validateBS, validateCF, validateIndex, validateIS, validateStockPrice
 
 
 def choiceStockPriceWithAnnouncementDate(announcementDate: str, spList: List[StockPrice]) -> Optional[StockPrice]:
@@ -70,60 +72,102 @@ def generateDataWithYahooAPI() -> List[Tuple[FinantialStatements, BalanceSheet]]
         raise ValueError('企業情報が登録されていません')
 
     for company in companyList:
+
         handler: yfinance.TickerHandler = yfinance.TickerHandler()
         handler.registTicker(company)
 
         dateList: List[str] = yfinance.getCompanyDate(handler)
         stockAmountList: Optional[List[Dict[str, int]]] = yfinance.getCompanyStockAmount(handler, dateList)
+        # print(stockAmountList)
 
         if stockAmountList is None:
             continue
 
         # 株価データを生成
-        spList: List[StockPrice] = []
-        spRequestList: List[AddStockPriceRequestType] = yfinance.getCompanyStockPrice(handler, company)
+        spList: List[StockPrice] = getPriceList(company['id'])
+        if(len(spList) == 0):
+            spRequestList: List[AddStockPriceRequestType] = yfinance.getCompanyStockPrice(handler, company)
 
-        stockPriceBar = tqdm(total = len(spRequestList))
-        stockPriceBar.set_description("{}の株価情報を登録中".format(company['name']))
-        for spRequest in spRequestList:
-            stockPriceBar.update(1)
-            sp = addPrice(spRequest)
-            if sp is not None:
+            stockPriceBar = tqdm(total = len(spRequestList))
+            stockPriceBar.set_description("{}の株価情報を登録中".format(company['name']))
+            for spRequest in spRequestList:
+                stockPriceBar.update(1)
+                sp = addPrice(spRequest)
+                validateStockPrice(sp)
                 spList.append(sp)
 
         # 各表のベースとなる財務諸表データを生成
-        fsList: List[FinantialStatements] = []
-        for date in dateList:
-            fs = addFinantialStatements({"companyID": company['id'], "announcementDate": date, "isFiscal": True})
-            fsList.append(fs)
+        fsList: List[FinantialStatements] = getFinantialStatementsList(company['id'])
+
+        if(len(fsList) == 0):
+            for date in dateList:
+                fs = addFinantialStatements({"companyID": company['id'], "announcementDate": date, "isFiscal": True})
+                validateFS(fs)
+                fsList.append(fs)
         
         # 財務諸表データをベースにしたバランスシートデータを生成
-        bsRequestList: List[AddBalanceSheetRequestType] = yfinance.getCompanyBS(handler, fsList, stockAmountList)
         bsList: List[BalanceSheet] = []
-        for bsRequest in bsRequestList:
-            bs = addBalanceSheet(company['id'], bsRequest)
-            bsList.append(bs)
+        for fs in fsList:
+            bsList.append(getBalanceSheetList(company['id'], fs['id']))
+
+        # FIXME: 邪悪
+        # flatten
+        bsList = list(itertools.chain.from_iterable(bsList))
+
+        if(len(bsList) == 0): 
+            bsRequestList: List[AddBalanceSheetRequestType] = yfinance.getCompanyBS(handler, fsList, stockAmountList)
+            for bsRequest in bsRequestList:
+                bs = addBalanceSheet(company['id'], bsRequest)
+                validateBS(bs)
+                bsList.append(bs)
         
         # 財務諸表データをベースにしたキャッシュ・フローデータを生成
-        cfRequestList: List[AddCashFlowRequestType] = yfinance.getCompanyCF(handler, fsList)
         cfList: List[Cashflow] = []
-        for cfRequest in cfRequestList:
-            cf = addCashFlow(company['id'], cfRequest)
-            cfList.append(cf)
+        for fs in fsList:
+            cfList.append(getCashFlowList(company['id'], fs['id']))
+
+        # FIXME: 邪悪
+        # flatten
+        cfList = list(itertools.chain.from_iterable(cfList))
+
+        if(len(cfList) == 0):
+            cfRequestList: List[AddCashFlowRequestType] = yfinance.getCompanyCF(handler, fsList)
+            for cfRequest in cfRequestList:
+                cf = addCashFlow(company['id'], cfRequest)
+                validateCF(cf)
+                cfList.append(cf)
 
         # 財務諸表データをベースにした損益計算書データを生成
-        isRequestList: List[AddIncomeStatementRequestType] = yfinance.getCompanyIS(handler, fsList)
         isList: List[IncomeStatement] = []
-        for isRequest in isRequestList:
-            istatement = addIncomeStatement(company['id'], isRequest)
-            isList.append(istatement)
+        for fs in fsList:
+            isList.append(getIncomeStatementList(company['id'], fs['id']))
+
+        # FIXME: 邪悪
+        # flatten
+        isList = list(itertools.chain.from_iterable(isList))
+
+        if(len(isList) == 0):
+            isRequestList: List[AddIncomeStatementRequestType] = yfinance.getCompanyIS(handler, fsList)
+            for isRequest in isRequestList:
+                istatement = addIncomeStatement(company['id'], isRequest)
+                validateIS(istatement)
+                isList.append(istatement)
 
         # 財務諸表データをベースにした指標データを生成
-        indexRequestList: List[AddIndexRequestType] = calcIndex(spList, fsList, bsList, cfList, isList)
         indexList: List[Index] = []
-        for indexRequest in indexRequestList:
-            index = addIndex(company['id'], indexRequest)
-            indexList.append(index)
+        for fs in fsList:
+            indexList.append(getIndexList(company['id'], fs['id']))
+
+        # FIXME: 邪悪
+        # flatten
+        indexList = list(itertools.chain.from_iterable(indexList))
+
+        if(len(indexList) == 0):
+            indexRequestList: List[AddIndexRequestType] = calcIndex(spList, fsList, bsList, cfList, isList)
+            for indexRequest in indexRequestList:
+                index = addIndex(company['id'], indexRequest)
+                validateIndex(index)
+                indexList.append(index)
         
         companyListBar.update(1)
 
